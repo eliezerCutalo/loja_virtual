8a728eec4432c754af8340925b03e331d571c3f5
Eliezer Cútalo <eliezercsj@gmail.com>
2019-07-25 15:16:17 -0300

    Iniciando carrinho

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/screens/home_screen.dart';
 import 'package:loja_virtual/screens/login_screen.dart';
 import 'package:loja_virtual/screens/singup_screen.dart';
@@ -6,27 +7,29 @@ import 'package:scoped_model/scoped_model.dart';
 
 import 'models/user_model.dart';
 
-void main(){
-  runApp(
-    new MyApp()
-  );
+void main() {
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScopedModel<UserModel>(
-      model: UserModel(),
-      child:  MaterialApp(
-      title: "Flutter's Clothing",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Color.fromARGB(255, 4, 125, 141)
-      ),
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-      
-    ),
-    );
+        model: UserModel(),
+        child: ScopedModelDescendant(
+          builder: (context, child, model) {
+            return ScopedModel<CartModel>(
+              model: CartModel(model),
+              child: MaterialApp(
+                title: "Flutter's Clothing",
+                theme: ThemeData(
+                    primarySwatch: Colors.blue,
+                    primaryColor: Color.fromARGB(255, 4, 125, 141)),
+                debugShowCheckedModeBanner: false,
+                home: HomeScreen(),
+              ),
+            );
+          },
+        ));
   }
 }
