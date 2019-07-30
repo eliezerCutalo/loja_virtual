efcd841134abb3e7d1545971ba91e14ca6b0c78c
el1 <eliezercsj@gmail.com>
2019-07-30 00:11:00 -0300

    Fazendo tela de pedido

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -91,6 +91,47 @@ class CartModel extends Model{
     notifyListeners();
   }
 
+  Future<String> finishOrder() async{
+    if(products.length == 0) return null;
+
+    isLoading = true;
+    notifyListeners();
+
+    double productsPrice = getProductsPrice();
+    double shipPrice = getShipPrice();
+    double discount = getDiscount();
+
+    DocumentReference refOrder = await Firestore.instance.collection("orders").add(
+      {
+        "clientId": user.firebaseUser.uid,
+        "products": products.map((cartProduct)=> cartProduct.toMap()).toList(),
+        "shipPrice": shipPrice,
+        "productsPrice": productsPrice,
+        "discount": discount,
+        "totalPrice": productsPrice - discount + shipPrice,
+        "status": 1
+      }
+    );
+
+    Firestore.instance.collection("users").document(user.firebaseUser.uid)
+      .collection("orders").document(refOrder.documentID).setData({
+        "orderId": refOrder.documentID
+      });
+    
+    QuerySnapshot query = await Firestore.instance.collection("users").document(user.firebaseUser.uid)
+      .collection("cart").getDocuments();
+
+    for(DocumentSnapshot doc in query.documents){
+      doc.reference.delete();
+    }
+    products.clear();
+    couponCode = null;
+    discountPercentage = 0;
+
+    isLoading = false;
+    notifyListeners();
+    return refOrder.documentID;
+  }
 
 
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/tabs/orders_tab.dart';
 import 'package:loja_virtual/tabs/products_tab.dart';
 import 'package:loja_virtual/widgets/cart_button.dart';
 import 'package:loja_virtual/widgets/custom_drawer.dart';
@@ -27,6 +28,15 @@ class HomeScreen extends StatelessWidget {
           drawer: CustomDrawer(_pageController),
           body: ProductsTab(),
           floatingActionButton: CartButton(),
+        ),
+        Container(color:  Colors.yellow,),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Meus pedidos"),
+            centerTitle: true,
+          ),
+          body: OrdersTab(),
+          drawer: CustomDrawer(_pageController),
         )
       ],
     );
