6fd9946de0281adaedaf7038f52fb99bed1a892b
Eliezer Cútalo <eliezercsj@gmail.com>
2019-07-31 16:56:16 -0300

    Finalizando parte de pedidos

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
 import 'package:loja_virtual/tabs/orders_tab.dart';
+import 'package:loja_virtual/tabs/places_tab.dart';
 import 'package:loja_virtual/tabs/products_tab.dart';
 import 'package:loja_virtual/widgets/cart_button.dart';
 import 'package:loja_virtual/widgets/custom_drawer.dart';
@@ -29,7 +30,14 @@ class HomeScreen extends StatelessWidget {
           body: ProductsTab(),
           floatingActionButton: CartButton(),
         ),
-        Container(color:  Colors.yellow,),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Lojas"),
+            centerTitle: true,
+          ),
+          body: PlacesTab(),
+          drawer: CustomDrawer(_pageController),
+        ),
         Scaffold(
           appBar: AppBar(
             title: Text("Meus pedidos"),
--- lib/tiles/order_tile.dart
+++ lib/tiles/order_tile.dart
@@ -21,6 +21,8 @@ class OrderTile extends StatelessWidget {
             if(!snapshot.hasData)
               return Center(child: CircularProgressIndicator(),);
             else{
+              int status = snapshot.data["status"];
+
               return Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
@@ -30,7 +32,23 @@ class OrderTile extends StatelessWidget {
                   SizedBox(height: 4.0,),
                   Text(
                     _buildProductsText(snapshot.data)
-                  )
+                  ),
+                  SizedBox(height: 4.0,),
+                  Text("Status do pedido:",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 4.0,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      _buildCircle("1", "Preparação", status, 1),
+                      Container(height: 1.0, width: 40.0, color: Colors.grey[500],),
+                      _buildCircle("2", "Transporte", status, 2),
+                      Container(height: 1.0, width: 40.0, color: Colors.grey[500],),
+                      _buildCircle("3", "Entrega", status, 3)
+                    ],
+                  ),
+
                 ],
               );
             }
@@ -50,5 +68,39 @@ class OrderTile extends StatelessWidget {
 
     return text;
 
+  }
+
+  Widget _buildCircle(String title, String subTitle, int status, int thisStatus){
+    Color backColor;
+    Widget child;
+    if(status < thisStatus){
+      backColor = Colors.grey[500];
+      child = Text(title, style: TextStyle(color: Colors.white),);
+    }else if(status == thisStatus){
+      backColor = Colors.blue;
+      child = Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Text(title, style: TextStyle(color: Colors.white),),
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          )
+        ],
+      );
+    }else{
+      backColor = Colors.green;
+      child = Icon(Icons.check);
+    }
+
+    return Column(
+      children: <Widget>[
+        CircleAvatar(
+          radius: 20.0,
+          backgroundColor: backColor,
+          child: child,
+        ),
+        Text(subTitle)
+      ],
+    );
   }
 }
