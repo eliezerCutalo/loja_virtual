38c390f2da7fa1be400d476da3bab31817e32002
Eliezer Cútalo <eliezercsj@gmail.com>
2019-08-01 10:38:53 -0300

    Finalizando loja virtual

--- lib/tiles/place_tile.dart
+++ lib/tiles/place_tile.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 //.reversed;tolist
 class PlaceTile extends StatelessWidget {
 
@@ -9,6 +10,61 @@ class PlaceTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          SizedBox(
+            height: 150.0,
+            child: Image.network(
+              snapshot.data["image"],
+              fit: BoxFit.cover,
+            ), 
+          ),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  snapshot.data["title"],
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                Text(
+                  snapshot.data["address"],
+                  textAlign: TextAlign.start,
+                )
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text("Ver no mapa"),
+                textColor: Colors.blue,
+                padding: EdgeInsets.zero,
+                onPressed: (){
+                  launch("https://www.google.com/maps/search/?api=1&query=${snapshot.data["lat"]},${snapshot.data["long"]}");
+                },
+
+              ),
+              FlatButton(
+                child: Text("Ligar"),
+                textColor: Colors.blue,
+                padding: EdgeInsets.zero,
+                onPressed: (){
+                  launch("tel: ${snapshot.data["phone"]}");
+                },
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
